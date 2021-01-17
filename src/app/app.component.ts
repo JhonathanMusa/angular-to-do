4518a15d97fc5986b312bfb449a90e08d83c6077
jhonathanmusa <jhonathansamu91@gmail.com>
2021-01-16 19:15:50 -0500

    adding events and data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'to-do';
+  act = ''
+
+  activities = [
+    "Work",
+    "Eat"
+  ]
+
+  addAct() {
+    this.activities.unshift(this.act)
+  }
+
+  deletAct(act: string) {
+    for (let x = 0; x < this.activities.length; x++)
+      if (this.activities[x] == this.act) {
+        this.activities.splice(x, 1)
+        return
+      }
+  }
 }
