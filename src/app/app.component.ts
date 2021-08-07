d399b61a1fe76ee130e783e4e781a28fca09063a
jhonathanmusa <jhonathansamu91@gmail.com>
2021-08-07 18:43:17 -0500

    updated form and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,13 +19,13 @@ export class AppComponent {
     this.activities.push(this.text.value)
   }
 
-  deletAct(index: number) {
-    for (let x = 0; x < this.activities.length; x++)
-      if (this.activities[x] == this.text.value) {
-        this.activities.splice(x, 1)
-        return
-      }
-  }
+  /*  deletAct(index: number) {
+     for (let x = 0; x < this.activities.length; x++)
+       if (this.activities[x] == this.text.value) {
+         this.activities.splice(x, 1)
+         return
+       }
+   } */
 
 
 }
